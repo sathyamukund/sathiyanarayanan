7d1aabec502043fadf1ae9cd6498ba37aa86b892
sathyamukund <104051662+sathyamukund@users.noreply.github.com>
2022-04-20 15:02:15 +0530

    Add files via upload

---
+++ day1/negativetopositive.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class negativetopositive {
+
+	public static void main(String[] args) {
+		 int a=-65;
+	        if(a<0) {
+	        	int positive = a * -1;
+	        	System.out.println(positive);
+	        }
+	}
+}
